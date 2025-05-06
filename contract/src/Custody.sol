9dad18cecedb751e49c6cb173a00f1aa73eb0fac
Sazonov Nikita <35502225+nksazonov@users.noreply.github.com>
2025-05-06 17:42:50 +0300

    fix(Custody): use SafeERC20 for ERC20 transfers (#53)

--- contract/src/Custody.sol
+++ contract/src/Custody.sol
@@ -8,6 +8,7 @@ import {IComparable} from "./interfaces/IComparable.sol";
 import {Channel, State, Allocation, ChannelStatus, StateIntent, Signature, Amount} from "./interfaces/Types.sol";
 import {Utils} from "./Utils.sol";
 import {IERC20} from "lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {EnumerableSet} from "lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 
 /**
@@ -17,6 +18,7 @@ import {EnumerableSet} from "lib/openzeppelin-contracts/contracts/utils/structs/
  */
 contract Custody is IChannel, IDeposit {
     using EnumerableSet for EnumerableSet.Bytes32Set;
+    using SafeERC20 for IERC20;
 
     // Errors
     error ChannelNotFound(bytes32 channelId);

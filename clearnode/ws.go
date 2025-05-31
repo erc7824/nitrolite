6450a8d60b4c271e60962614b4e74a97286a1202
Dmytro Steblyna <dsteblyna@openware.center>
2025-05-31 16:07:07 +0200

    update

--- clearnode/ws.go
+++ clearnode/ws.go
@@ -478,16 +478,8 @@ func (h *UnifiedWSHandler) sendErrorResponse(sender string, rpc *RPCMessage, con
 	if rpc != nil && rpc.Req != nil {
 		reqID = rpc.Req.RequestID
 	}
-	var messageBody string
-	if rpc != nil {
-		messageBodyBytes, err := json.Marshal(rpc)
-		if err != nil {
-			messageBody = "could not marshal rpc message"
-		} else {
-			messageBody = string(messageBodyBytes)
-		}
-	}
-	log.Printf("error: %s, wallet: %s, method: %s, request: %s", errMsg, sender, rpc.Req.Method, messageBody)
+
+	log.Printf("error: %s, wallet: %s, method: %s, request: %s", errMsg, sender, rpc.Req.Method)
 
 	response := CreateResponse(reqID, "error", []any{map[string]any{
 		"error": errMsg,

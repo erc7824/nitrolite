dfe4d0d10249e0a6359842a93365d615efaad9eb
nksazonov <nsazonov@openware.com>
2025-07-16 17:54:39 +0300

    style(contract): add empty lines at the end

--- contract/test/TestUtils.sol
+++ contract/test/TestUtils.sol
@@ -10,14 +10,14 @@ library TestUtils {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
         return abi.encodePacked(r, s, v);
     }
-    
+
     function signEIP191(Vm vm, uint256 privateKey, bytes32 messageHash) external pure returns (bytes memory) {
         // Apply EIP-191 prefix and sign
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(messageHash);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, ethSignedMessageHash);
         return abi.encodePacked(r, s, v);
     }
-    
+
     function signEIP712(Vm vm, uint256 privateKey, bytes32 domainSeparator, bytes32 structHash) external pure returns (bytes memory) {
         // Apply EIP-712 prefix and sign
         bytes32 typedDataHash = MessageHashUtils.toTypedDataHash(domainSeparator, structHash);

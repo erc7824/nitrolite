a597691cb41e3c55841af401990df7c07ddde38a
Alessio Treglia <al@essio.dev>
2026-02-18 14:05:09 +0000

    test cases

---
+++ pkg/blockchain/evm/client_test.go
@@ -0,0 +1,174 @@
+package evm
+
+import (
+	"math/big"
+	"strings"
+	"testing"
+
+	"github.com/erc7824/nitrolite/pkg/sign"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+// MockSigner implements sign.Signer
+type MockSigner struct {
+	mock.Mock
+}
+
+func (m *MockSigner) Sign(data []byte) (sign.Signature, error) {
+	args := m.Called(data)
+	return args.Get(0).(sign.Signature), args.Error(1)
+}
+
+func (m *MockSigner) PublicKey() sign.PublicKey {
+	args := m.Called()
+	return args.Get(0).(sign.PublicKey)
+}
+
+// MockPublicKey implements sign.PublicKey
+type MockPublicKey struct {
+	addr common.Address
+}
+
+func (m *MockPublicKey) Address() sign.Address {
+	return sign.NewEthereumAddress(m.addr)
+}
+
+func (m *MockPublicKey) Bytes() []byte {
+	return m.addr.Bytes()
+}
+
+func TestNewClient(t *testing.T) {
+	t.Parallel()
+	mockEVMClient := new(MockEVMClient)
+	mockAssetStore := new(MockAssetStore)
+	mockSigner := new(MockSigner)
+
+	// Setup mock signer
+	privKey, err := crypto.GenerateKey()
+	require.NoError(t, err)
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+
+	contractAddress := common.HexToAddress("0x123")
+	nodeAddress := "0x456"
+	blockchainID := uint64(1337)
+
+	// NewClient calls NewChannelHub which doesn't make external calls in standard abigen,
+	// but let's see. If it checks code, we need CodeAt.
+	// Assuming standard abigen, it just returns the struct.
+
+	client, err := NewClient(
+		contractAddress,
+		mockEVMClient,
+		mockSigner,
+		blockchainID,
+		nodeAddress,
+		mockAssetStore,
+	)
+
+	require.NoError(t, err)
+	assert.NotNil(t, client)
+}
+
+func TestClient_GetAccountsBalances(t *testing.T) {
+	t.Parallel()
+	mockEVMClient := new(MockEVMClient)
+	mockAssetStore := new(MockAssetStore)
+	mockSigner := new(MockSigner)
+
+	// Setup mock signer
+	privKey, _ := crypto.GenerateKey()
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+
+	contractAddress := common.HexToAddress("0xContract")
+	client, _ := NewClient(contractAddress, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
+
+	accounts := []string{"0xUser1", "0xUser2"}
+	tokens := []string{"0xToken1"}
+
+	// Mock CallContract for GetAccountBalance
+	// We need to match the call data. Since we can't easily reproduce abi packing here without the abi,
+	// we will mock CallContract to return a generic success for any call.
+	// In reality, we should check the method ID.
+	// GetAccountBalance(bytes32 channelID, address token) returns (uint256)
+	// Wait, the client calls c.contract.GetAccountBalance(nil, accountAddr, tokenAddr).
+
+	// Mock successful return (uint256 = 100)
+	ret := common.LeftPadBytes(big.NewInt(100).Bytes(), 32)
+	mockEVMClient.On("CallContract", mock.Anything, mock.Anything, mock.Anything).Return(ret, nil)
+
+	balances, err := client.GetAccountsBalances(accounts, tokens)
+	require.NoError(t, err)
+	assert.Len(t, balances, 2)
+	assert.Len(t, balances[0], 1)
+	assert.Equal(t, "100", balances[0][0].String())
+	assert.Equal(t, "100", balances[1][0].String())
+}
+
+func TestClient_GetNodeBalance(t *testing.T) {
+	t.Parallel()
+	mockEVMClient := new(MockEVMClient)
+	mockAssetStore := new(MockAssetStore)
+	mockSigner := new(MockSigner)
+
+	// Setup mock signer
+	privKey, _ := crypto.GenerateKey()
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+
+	client, _ := NewClient(common.Address{}, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
+
+	token := "0xToken"
+	mockAssetStore.On("GetTokenDecimals", uint64(1), token).Return(uint8(18), nil)
+
+	// Mock GetAccountBalance call
+	ret := common.LeftPadBytes(big.NewInt(1000000000000000000).Bytes(), 32) // 1 ETH
+	mockEVMClient.On("CallContract", mock.Anything, mock.Anything, mock.Anything).Return(ret, nil)
+
+	balance, err := client.GetNodeBalance(token)
+	require.NoError(t, err)
+	assert.Equal(t, "1", balance.String())
+}
+
+func TestClient_GetOpenChannels(t *testing.T) {
+	t.Parallel()
+	mockEVMClient := new(MockEVMClient)
+	mockAssetStore := new(MockAssetStore)
+	mockSigner := new(MockSigner)
+
+	// Setup mock signer
+	privKey, err1 := crypto.GenerateKey()
+	require.NoError(t, err1)
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+
+	client, err2 := NewClient(common.Address{}, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
+	require.NoError(t, err2)
+
+	// Mock GetOpenChannels return: bytes32[]
+	// Let's return 1 channel ID
+	chanID := common.HexToHash("0x1234")
+	// ABI encoding for dynamic array: offset, length, data
+	// offset to data (32 bytes)
+	offset := common.LeftPadBytes(big.NewInt(32).Bytes(), 32)
+	// length (1)
+	length := common.LeftPadBytes(big.NewInt(1).Bytes(), 32)
+	// data (chanID)
+	data := chanID.Bytes()
+
+	ret := append(offset, length...)
+	ret = append(ret, data...)
+
+	mockEVMClient.On("CallContract", mock.Anything, mock.Anything, mock.Anything).Return(ret, nil)
+
+	channels, err3 := client.GetOpenChannels("0xUser")
+	require.NoError(t, err3)
+	assert.Len(t, channels, 1)
+	assert.Equal(t, strings.ToLower(hexutil.Encode(chanID[:])), strings.ToLower(channels[0]))
+}
--- pkg/blockchain/evm/listener_test.go
+++ pkg/blockchain/evm/listener_test.go
@@ -2,42 +2,196 @@ package evm
 
 import (
 	"context"
+	"math/big"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/erc7824/nitrolite/pkg/core"
+	"github.com/erc7824/nitrolite/pkg/log"
+	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
-
-	"github.com/erc7824/nitrolite/pkg/log"
 )
 
-func TestReconcileBlockRange(t *testing.T) {
-	t.Skip("for manual testing only")
+// MockSubscription implements ethereum.Subscription
+type MockSubscription struct {
+	errChan chan error
+	unsub   func()
+}
 
-	blockchainRPC := "CHANGE_ME"
-	contractAddress := common.HexToAddress("CHANGE_ME")
+func (m *MockSubscription) Err() <-chan error {
+	return m.errChan
+}
 
-	client, err := ethclient.Dial(blockchainRPC)
-	require.NoError(t, err, "Failed to connect to Ethereum client")
+func (m *MockSubscription) Unsubscribe() {
+	if m.unsub != nil {
+		m.unsub()
+	}
+}
 
-	chainID, err := client.ChainID(context.TODO())
-	require.NoError(t, err, "Failed to get chain ID")
-
-	historicalCh := make(chan types.Log, 100)
+func TestNewListener(t *testing.T) {
+	t.Parallel()
+	mockClient := new(MockEVMClient)
 	logger := log.NewNoopLogger()
+	addr := common.HexToAddress("0x123")
 
-	listener := NewListener(
-		contractAddress,
-		client,
-		chainID.Uint64(),
-		499, // blockStep
-		logger,
-		nil, // eventHandler not needed for this test
-		nil, // getLatestEvent not needed for this test
-	)
+	l := NewListener(addr, mockClient, 1, 100, logger, nil, nil)
+	require.NotNil(t, l)
+	assert.Equal(t, addr, l.contractAddress)
+	assert.Equal(t, uint64(1), l.blockchainID)
+	assert.Equal(t, uint64(100), l.blockStep)
+}
 
-	// Call reconcileBlockRange with appropriate parameters
-	// currentBlock, lastBlock, lastIndex, historicalCh
-	listener.reconcileBlockRange(31530000, 31527936, 0, historicalCh)
+func TestListener_Listen_CurrentEvents(t *testing.T) {
+	t.Parallel()
+	mockClient := new(MockEVMClient)
+	logger := log.NewNoopLogger()
+	addr := common.HexToAddress("0x123")
+
+	// Setup latest event getter (start from 0)
+	getLatestEvent := func(contractAddress string, networkID uint64) (core.BlockchainEvent, error) {
+		return core.BlockchainEvent{BlockNumber: 0, LogIndex: 0}, nil
+	}
+
+	// Channel to signal event handling
+	eventHandled := make(chan struct{})
+	handleEvent := func(ctx context.Context, log types.Log) {
+		close(eventHandled)
+	}
+
+	listener := NewListener(addr, mockClient, 1, 100, logger, handleEvent, getLatestEvent)
+
+	// Mock SubscribeFilterLogs
+	sub := &MockSubscription{
+		errChan: make(chan error),
+		unsub:   func() {},
+	}
+
+	// Mock HeaderByNumber
+	mockClient.On("SubscribeFilterLogs", mock.Anything, mock.Anything, mock.Anything).
+		Run(func(args mock.Arguments) {
+			ch := args.Get(2).(chan<- types.Log)
+			// Send a log immediately
+			ch <- types.Log{BlockNumber: 10, Index: 1}
+		}).
+		Return(sub, nil)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	go listener.Listen(ctx, func(err error) {})
+
+	select {
+	case <-eventHandled:
+		// Success
+	case <-time.After(1 * time.Second):
+		t.Fatal("timeout waiting for event")
+	}
+}
+
+func TestListener_ReconcileBlockRange(t *testing.T) {
+	t.Parallel()
+	mockClient := new(MockEVMClient)
+	logger := log.NewNoopLogger()
+	addr := common.HexToAddress("0x123")
+
+	listener := NewListener(addr, mockClient, 1, 10, logger, nil, nil)
+
+	// Setup FilterLogs mock
+	// We expect a range fetch. start=100, step=10 -> end=110. current=120.
+	// 1. 100-110
+	// 2. 111-120
+
+	logs1 := []types.Log{{BlockNumber: 105, Index: 0}}
+	logs2 := []types.Log{{BlockNumber: 115, Index: 0}}
+
+	mockClient.On("FilterLogs", mock.Anything, mock.MatchedBy(func(q ethereum.FilterQuery) bool {
+		return q.FromBlock.Uint64() == 100 && q.ToBlock.Uint64() == 110
+	})).Return(logs1, nil)
+
+	mockClient.On("FilterLogs", mock.Anything, mock.MatchedBy(func(q ethereum.FilterQuery) bool {
+		return q.FromBlock.Uint64() == 111 && q.ToBlock.Uint64() == 120
+	})).Return(logs2, nil)
+
+	historicalCh := make(chan types.Log, 10)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		listener.reconcileBlockRange(120, 100, 0, historicalCh)
+		close(historicalCh)
+	}()
+
+	var receivedLogs []types.Log
+	for l := range historicalCh {
+		receivedLogs = append(receivedLogs, l)
+	}
+	wg.Wait()
+
+	assert.Len(t, receivedLogs, 2)
+	assert.Equal(t, uint64(105), receivedLogs[0].BlockNumber)
+	assert.Equal(t, uint64(115), receivedLogs[1].BlockNumber)
+}
+
+func TestListener_Listen_HistoricalAndCurrent(t *testing.T) {
+	t.Parallel()
+	mockClient := new(MockEVMClient)
+	logger := log.NewNoopLogger()
+	addr := common.HexToAddress("0x123")
+
+	// Start from block 100
+	getLatestEvent := func(contractAddress string, networkID uint64) (core.BlockchainEvent, error) {
+		return core.BlockchainEvent{BlockNumber: 100, LogIndex: 0}, nil
+	}
+
+	receivedCount := 0
+	doneCh := make(chan struct{})
+
+	handleEvent := func(ctx context.Context, log types.Log) {
+		receivedCount++
+		if receivedCount >= 2 { // Expect 1 historical + 1 current
+			select {
+			case <-doneCh:
+			default:
+				close(doneCh)
+			}
+		}
+	}
+
+	listener := NewListener(addr, mockClient, 1, 10, logger, handleEvent, getLatestEvent)
+
+	// Mock HeaderByNumber (current tip is 110)
+	currentHeader := &types.Header{Number: big.NewInt(110)}
+	mockClient.On("HeaderByNumber", mock.Anything, (*big.Int)(nil)).Return(currentHeader, nil)
+
+	// Mock FilterLogs (100-110)
+	histLogs := []types.Log{{BlockNumber: 105, Index: 0}}
+	mockClient.On("FilterLogs", mock.Anything, mock.Anything).Return(histLogs, nil)
+
+	// Mock SubscribeFilterLogs
+	sub := &MockSubscription{errChan: make(chan error)}
+	mockClient.On("SubscribeFilterLogs", mock.Anything, mock.Anything, mock.Anything).
+		Run(func(args mock.Arguments) {
+			ch := args.Get(2).(chan<- types.Log)
+			// Send a current log
+			ch <- types.Log{BlockNumber: 111, Index: 0}
+		}).
+		Return(sub, nil)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	go listener.Listen(ctx, func(err error) {})
+
+	select {
+	case <-doneCh:
+		// Success
+	case <-time.After(2 * time.Second):
+		t.Fatal("timeout waiting for events")
+	}
 }

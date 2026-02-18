7e487c1e17d7458ba844975a0fa75d0528a4b6c1
Alessio Treglia <al@essio.dev>
2026-02-18 14:58:58 +0000

    apply suggestions

--- pkg/blockchain/evm/client_test.go
+++ pkg/blockchain/evm/client_test.go
@@ -31,7 +31,8 @@ func (m *MockSigner) PublicKey() sign.PublicKey {
 
 // MockPublicKey implements sign.PublicKey
 type MockPublicKey struct {
-	addr common.Address
+	addr     common.Address
+	pubBytes []byte
 }
 
 func (m *MockPublicKey) Address() sign.Address {
@@ -39,7 +40,7 @@ func (m *MockPublicKey) Address() sign.Address {
 }
 
 func (m *MockPublicKey) Bytes() []byte {
-	return m.addr.Bytes()
+	return m.pubBytes
 }
 
 func TestNewClient(t *testing.T) {
@@ -52,16 +53,14 @@ func TestNewClient(t *testing.T) {
 	privKey, err := crypto.GenerateKey()
 	require.NoError(t, err)
 	addr := crypto.PubkeyToAddress(privKey.PublicKey)
-	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+	pubBytes := crypto.FromECDSAPub(&privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr, pubBytes: pubBytes})
 
 	contractAddress := common.HexToAddress("0x123")
 	nodeAddress := "0x456"
 	blockchainID := uint64(1337)
 
-	// NewClient calls NewChannelHub which doesn't make external calls in standard abigen,
-	// but let's see. If it checks code, we need CodeAt.
-	// Assuming standard abigen, it just returns the struct.
-
+	// Mock models simulate an EVM client where NewClient/NewChannelHub return a local struct without external calls
 	client, err := NewClient(
 		contractAddress,
 		mockEVMClient,
@@ -73,6 +72,8 @@ func TestNewClient(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.NotNil(t, client)
+
+	mock.AssertExpectationsForObjects(t, mockEVMClient, mockAssetStore, mockSigner)
 }
 
 func TestClient_GetAccountsBalances(t *testing.T) {
@@ -85,7 +86,8 @@ func TestClient_GetAccountsBalances(t *testing.T) {
 	privKey, err := crypto.GenerateKey()
 	require.NoError(t, err)
 	addr := crypto.PubkeyToAddress(privKey.PublicKey)
-	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+	pubBytes := crypto.FromECDSAPub(&privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr, pubBytes: pubBytes})
 
 	contractAddress := common.HexToAddress("0xContract")
 	client, err := NewClient(contractAddress, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
@@ -93,13 +95,6 @@ func TestClient_GetAccountsBalances(t *testing.T) {
 
 	accounts := []string{"0xUser1", "0xUser2"}
 	tokens := []string{"0xToken1"}
-
-	// Mock CallContract for GetAccountBalance
-	// We need to match the call data. Since we can't easily reproduce abi packing here without the abi,
-	// we will mock CallContract to return a generic success for any call.
-	// In reality, we should check the method ID.
-	// GetAccountBalance(bytes32 channelID, address token) returns (uint256)
-	// Wait, the client calls c.contract.GetAccountBalance(nil, accountAddr, tokenAddr).
 
 	// Mock successful return (uint256 = 100)
 	ret := common.LeftPadBytes(big.NewInt(100).Bytes(), 32)
@@ -111,6 +106,8 @@ func TestClient_GetAccountsBalances(t *testing.T) {
 	assert.Len(t, balances[0], 1)
 	assert.Equal(t, "100", balances[0][0].String())
 	assert.Equal(t, "100", balances[1][0].String())
+
+	mock.AssertExpectationsForObjects(t, mockEVMClient, mockAssetStore, mockSigner)
 }
 
 func TestClient_GetNodeBalance(t *testing.T) {
@@ -120,11 +117,14 @@ func TestClient_GetNodeBalance(t *testing.T) {
 	mockSigner := new(MockSigner)
 
 	// Setup mock signer
-	privKey, _ := crypto.GenerateKey()
+	privKey, err := crypto.GenerateKey()
+	require.NoError(t, err)
 	addr := crypto.PubkeyToAddress(privKey.PublicKey)
-	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+	pubBytes := crypto.FromECDSAPub(&privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr, pubBytes: pubBytes})
 
-	client, _ := NewClient(common.Address{}, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
+	client, err := NewClient(common.Address{}, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
+	require.NoError(t, err)
 
 	token := "0xToken"
 	mockAssetStore.On("GetTokenDecimals", uint64(1), token).Return(uint8(18), nil)
@@ -136,6 +136,8 @@ func TestClient_GetNodeBalance(t *testing.T) {
 	balance, err := client.GetNodeBalance(token)
 	require.NoError(t, err)
 	assert.Equal(t, "1", balance.String())
+
+	mock.AssertExpectationsForObjects(t, mockEVMClient, mockAssetStore, mockSigner)
 }
 
 func TestClient_GetOpenChannels(t *testing.T) {
@@ -145,13 +147,14 @@ func TestClient_GetOpenChannels(t *testing.T) {
 	mockSigner := new(MockSigner)
 
 	// Setup mock signer
-	privKey, err1 := crypto.GenerateKey()
-	require.NoError(t, err1)
+	privKey, err := crypto.GenerateKey()
+	require.NoError(t, err)
 	addr := crypto.PubkeyToAddress(privKey.PublicKey)
-	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr})
+	pubBytes := crypto.FromECDSAPub(&privKey.PublicKey)
+	mockSigner.On("PublicKey").Return(&MockPublicKey{addr: addr, pubBytes: pubBytes})
 
-	client, err2 := NewClient(common.Address{}, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
-	require.NoError(t, err2)
+	client, err := NewClient(common.Address{}, mockEVMClient, mockSigner, 1, "0xNode", mockAssetStore)
+	require.NoError(t, err)
 
 	// Mock GetOpenChannels return: bytes32[]
 	// Let's return 1 channel ID
@@ -169,8 +172,10 @@ func TestClient_GetOpenChannels(t *testing.T) {
 
 	mockEVMClient.On("CallContract", mock.Anything, mock.Anything, mock.Anything).Return(ret, nil)
 
-	channels, err3 := client.GetOpenChannels("0xUser")
-	require.NoError(t, err3)
+	channels, err := client.GetOpenChannels("0xUser")
+	require.NoError(t, err)
 	assert.Len(t, channels, 1)
 	assert.Equal(t, strings.ToLower(hexutil.Encode(chanID[:])), strings.ToLower(channels[0]))
+
+	mock.AssertExpectationsForObjects(t, mockEVMClient, mockAssetStore, mockSigner)
 }
